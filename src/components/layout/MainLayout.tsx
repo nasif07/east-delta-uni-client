203bde6d5f40224d647f9c7e6ae1ef0c883156d9
nasif07 <nasifulislamnasif23@gmail.com>
2025-10-23 15:31:41 +0600

    :

--- src/components/layout/MainLayout.tsx
+++ src/components/layout/MainLayout.tsx
@@ -1,6 +1,7 @@
 import { Layout, Menu } from "antd";
 import { Outlet } from "react-router-dom";
-import { adminSliderItems } from "../../routes/admin.routes";
+import { sidebarItemsGenerator } from "../../utils/sidebarItemsGenerator";
+import { adminPaths } from "../../routes/admin.routes";
 const { Header, Content, Footer, Sider } = Layout;
 const MainLayout = () => {
   return (
@@ -29,7 +30,7 @@ const MainLayout = () => {
           theme="dark"
           mode="inline"
           defaultSelectedKeys={["4"]}
-          items={adminSliderItems}
+          items={sidebarItemsGenerator(adminPaths, "admin")}
         />
       </Sider>
       <Layout>
---
+++ src/utils/routesGenerator.ts
@@ -0,0 +1,21 @@
+import type { TRoute, TUserPath } from "../types";
+export const routeGenerator = (items: TUserPath[]) => {
+  const routes = items.reduce((acc: TRoute[], item) => {
+    if (item.path && item.element) {
+      acc.push({
+        path: item.path,
+        element: item.element,
+      });
+    } else if (item.children) {
+      item.children.forEach((child) => {
+        acc.push({
+          path: child.path!,
+          element: child.element,
+        });
+      });
+    }
+    console.log(acc);
+    return acc;
+  }, []);
+  return routes;
+};
