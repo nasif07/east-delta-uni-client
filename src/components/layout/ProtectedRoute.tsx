36c583c5ef88bb76525b3d19f93c95664658f611
nasif07 <nasifulislamnasif23@gmail.com>
2025-10-25 16:00:22 +0600

    added protected route

---
+++ src/components/layout/ProtectedRoute.tsx
@@ -0,0 +1,13 @@
+import { type ReactNode } from "react";
+import { useAppSelector } from "../../redux/hooks";
+import { Navigate } from "react-router-dom";
+
+const ProtectedRoute = ({ children }: { children: ReactNode }) => {
+  const { token } = useAppSelector((state) => state.auth);
+  if (!token) {
+    return <Navigate to="/login" replace={true} />;
+  }
+  return children;
+};
+
+export default ProtectedRoute;
